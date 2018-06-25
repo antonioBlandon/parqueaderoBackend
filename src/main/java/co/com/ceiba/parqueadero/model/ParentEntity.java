09346ace38912afbd06b150fc794ad6af71322ac
Jose Blandon <jose.blandon@ceiba.com.co>
2018-06-25 12:18:06 -0500

    Se agrego modelo de la base de datos

---
+++ src/main/java/co/com/ceiba/parqueadero/model/ParentEntity.java
@@ -0,0 +1,32 @@
+package co.com.ceiba.parqueadero.model;
+
+import java.io.Serializable;
+
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Access;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable {
+
+	private static final long serialVersionUID = -7176961818440481688L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id", unique=true, nullable=false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
